db0f29520e4cb4e5beb867180d27d735edcb684f
HarshDhiman001 <harsh220000@gmail.com>
2023-04-06 17:16:13 +0530

    updated

--- php/slider.php
+++ php/slider.php
@@ -14,30 +14,29 @@
     <title>Hello, world!</title>
   </head>
   <body>
-  <center>
-
+<center>
 <div class="container" id="slider">
   <div id="carouselExampleControls" class="carousel slide" data-ride="carousel">
     <div class="carousel-inner">
       <div class="carousel-item active">
-    <label>Get up to ₹3000.00 instant savings on selected Apple Watch models with eligible HDFC Bank cards.‡ Shop Apple Watch</label>
+      <label>This clone site is designed and developed by Mr. Harsh Dhiman</label>
+      </div>
+      <div class="carousel-item">
+      <label>Get up to ₹8000 instant savings on selected products with eligible HDFC Bank cards.</label>
+      </div>
+      <div class="carousel-item">
+      <label>Get up to ₹6000.00 instant savings on selected iPhone models with eligible HDFC Bank cards.</label>
+      </div>
+    </div>
+  <a class="carousel-control-prev" href="#carouselExampleControls" role="button" data-slide="prev">
+    <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+    <span class="sr-only">Previous</span>
+  </a>
+  <a class="carousel-control-next" href="#carouselExampleControls" role="button" data-slide="next">
+    <span class="carousel-control-next-icon" aria-hidden="true"></span>
+    <span class="sr-only">Next</span>
+  </a>
   </div>
-  <div class="carousel-item">
-    <label>Get up to ₹8000 instant savings on selected products with eligible HDFC Bank cards.</label>
-  </div>
-  <div class="carousel-item">
-    <label>Get up to ₹6000.00 instant savings on selected iPhone models with eligible HDFC Bank cards.</label>
-  </div>
-</div>
-<a class="carousel-control-prev" href="#carouselExampleControls" role="button" data-slide="prev">
-  <span class="carousel-control-prev-icon" aria-hidden="true"></span>
-  <span class="sr-only">Previous</span>
-</a>
-<a class="carousel-control-next" href="#carouselExampleControls" role="button" data-slide="next">
-  <span class="carousel-control-next-icon" aria-hidden="true"></span>
-  <span class="sr-only">Next</span>
-</a>
-</div>
 </div>
 </center>
     <center>
